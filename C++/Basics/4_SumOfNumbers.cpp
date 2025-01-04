5e4122f09e6f19b177f230543f069903f5798183
Aditya Amol Patil <157286761+Aditya948351@users.noreply.github.com>
2025-01-04 15:18:01 +0530

    Create 4_SumOfNumbers.cpp

---
+++ C++/Basics/4_SumOfNumbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n,sum = 0;
+    cout<<"Enter the number:\t";
+    cin>>num;
+//  cout(n(n+1)/2)
+    for(int i=0;i<=n;i++) {
+        sum = sum+i;
+    }
+}
+
+//sum of n natural numbers.
+//n(n+1)/2
+//sum of Squares of n natural numbers
+//[n(n+1)(2n+1)]/6
