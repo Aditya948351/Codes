a3fd758d4a7abb8b3d66121362eeee3df3934a30
Aditya Amol Patil <157286761+Aditya948351@users.noreply.github.com>
2025-01-04 14:54:42 +0530

    Update 3_MultiplicationTable.cpp

--- C++/Basics/3_MultiplicationTable.cpp
+++ C++/Basics/3_MultiplicationTable.cpp
@@ -1,1 +1,12 @@
+#include<iostream>
+using namespace std;
 
+int main() {
+    int num;
+    cout<<"Enter the number:\t";
+    cin>>num;
+
+    for(int i=0;i<11;i+=1) {
+        cout<< num <<" * "<< i << " = "<<num*i<<endl;
+    }
+}
