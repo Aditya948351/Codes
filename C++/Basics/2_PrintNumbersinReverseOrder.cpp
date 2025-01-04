42fcfb2fa7c12af9c4b6fffd094663ab507ca7ba
Aditya Amol Patil <157286761+Aditya948351@users.noreply.github.com>
2025-01-04 14:40:09 +0530

    Add files via upload

---
+++ C++/Basics/2_PrintNumbersinReverseOrder.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+    int main() {
+    int num,i ;
+    cout<<"Enter the Number";
+    cin>>num;
+
+    for(i=num;i>=1; i = i - 1) {
+        cout<<i<<" ";
+    }
+}
