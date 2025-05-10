497826468a61371ed5f9c678c8e6cbed1f49b98b
AdityaPatil948351 <adityapatil9721@gmail.com>
2025-05-10 16:06:46 +0530

    added Armgstrong Number

--- C++/Basics/11_ArmgstrongNo.cpp
+++ C++/Basics/11_ArmgstrongNo.cpp
@@ -1,6 +1,35 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+
+//sum of cubes of the digits of the number is the number itself.
 int main() {
-  
+    int num, original, sum = 0, digits = 0;
+    cout << "Enter a number: ";
+    cin >> num;
+    original = num;
+
+    // Count digits
+    int temp = num;
+    while (temp > 0) {
+        digits++;
+        temp /= 10;
+    }
+
+    temp = num;
+    while (temp > 0) {
+        int digit = temp % 10;
+        int power = 1;
+        for (int i = 0; i < digits; i++)
+            power *= digit;
+        sum += power;
+        temp /= 10;
+    }
+
+    if (sum == original)
+        cout << num << " is an Armstrong number." << endl;
+    else
+        cout << num << " is not an Armstrong number." << endl;
+
+    return 0;
 }
